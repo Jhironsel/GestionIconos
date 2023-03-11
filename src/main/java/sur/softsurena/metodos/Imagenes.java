b9ee1a38ffacb8b47c59b3c982bb35fb5198e0b3
jhironsel <jhironseld@gmail.com>
2023-03-11 13:04:26 -0400

    Documentamos los metodos de JAVAFX por el momento.

--- src/main/java/sur/softsurena/metodos/Imagenes.java
+++ src/main/java/sur/softsurena/metodos/Imagenes.java
@@ -5,16 +5,16 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.image.PixelWriter;
-import javafx.scene.image.WritableImage;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.BackgroundPosition;
-import javafx.scene.layout.BackgroundRepeat;
-import javafx.scene.layout.BackgroundSize;
+//import javafx.scene.image.Image;
+//import javafx.scene.image.ImageView;
+//import javafx.scene.image.PixelWriter;
+//import javafx.scene.image.WritableImage;
+//import javafx.scene.layout.AnchorPane;
+//import javafx.scene.layout.Background;
+//import javafx.scene.layout.BackgroundImage;
+//import javafx.scene.layout.BackgroundPosition;
+//import javafx.scene.layout.BackgroundRepeat;
+//import javafx.scene.layout.BackgroundSize;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 
@@ -48,52 +48,52 @@ public class Imagenes {
         return url;
     }
 
-    /**
-     * Metodo utilizado para cambiar los fondos de los anchorPane.
-     *
-     * Luego lo pondré en prueba con otro tipo de componente de la JAVAFX.
-     *
-     * @param anchorPane Es un objecto de la clase AnchorPane.
-     * @param ruta Es la ruta de la imagen. Este es un ejemplo:
-     * "Imagenes/Usuario.png".
-     * @param br Permite definir si la imagen se repite en el eje x.
-     * @param br2 Permite definir si la imagen se repite en el eje y.
-     * @param bp Define si la posicion en la imagen en el anchorPane.
-     */
-    public static void cambiarBackGroundAnchorPane(AnchorPane anchorPane,
-            String ruta, BackgroundRepeat br, BackgroundRepeat br2,
-            BackgroundPosition bp) {
-
-        BackgroundSize bs = new BackgroundSize(1, 1, true, true, false, false);
-
-        Image image = new Image(new Imagenes().dame(ruta));
-
-        BackgroundImage backgroundImage
-                = new BackgroundImage(image, br, br2, bp, bs);
-
-        Background background = new Background(backgroundImage);
-        anchorPane.setBackground(background);
-    }
+//    /**
+//     * Metodo utilizado para cambiar los fondos de los anchorPane.
+//     *
+//     * Luego lo pondré en prueba con otro tipo de componente de la JAVAFX.
+//     *
+//     * @param anchorPane Es un objecto de la clase AnchorPane.
+//     * @param ruta Es la ruta de la imagen. Este es un ejemplo:
+//     * "Imagenes/Usuario.png".
+//     * @param br Permite definir si la imagen se repite en el eje x.
+//     * @param br2 Permite definir si la imagen se repite en el eje y.
+//     * @param bp Define si la posicion en la imagen en el anchorPane.
+//     */
+//    public static void cambiarBackGroundAnchorPane(AnchorPane anchorPane,
+//            String ruta, BackgroundRepeat br, BackgroundRepeat br2,
+//            BackgroundPosition bp) {
+//
+//        BackgroundSize bs = new BackgroundSize(1, 1, true, true, false, false);
+//
+//        Image image = new Image(new Imagenes().dame(ruta));
+//
+//        BackgroundImage backgroundImage
+//                = new BackgroundImage(image, br, br2, bp, bs);
+//
+//        Background background = new Background(backgroundImage);
+//        anchorPane.setBackground(background);
+//    }
 
     private InputStream dame(String ruta) {
         return getClass().getResourceAsStream(ruta);
     }
 
-    public static Image convertToFxImage(String nombreImagenConExtension) throws IOException {
-        
-        BufferedImage image = ImageIO.read(new Imagenes().dame(nombreImagenConExtension));
-
-        WritableImage wr = null;
-        if (image != null) {
-            wr = new WritableImage(image.getWidth(), image.getHeight());
-            PixelWriter pw = wr.getPixelWriter();
-            for (int x = 0; x < image.getWidth(); x++) {
-                for (int y = 0; y < image.getHeight(); y++) {
-                    pw.setArgb(x, y, image.getRGB(x, y));
-                }
-            }
-        }
-
-        return new ImageView(wr).getImage();
-    }
+//    public static Image convertToFxImage(String nombreImagenConExtension) throws IOException {
+//        
+//        BufferedImage image = ImageIO.read(new Imagenes().dame(nombreImagenConExtension));
+//
+//        WritableImage wr = null;
+//        if (image != null) {
+//            wr = new WritableImage(image.getWidth(), image.getHeight());
+//            PixelWriter pw = wr.getPixelWriter();
+//            for (int x = 0; x < image.getWidth(); x++) {
+//                for (int y = 0; y < image.getHeight(); y++) {
+//                    pw.setArgb(x, y, image.getRGB(x, y));
+//                }
+//            }
+//        }
+//
+//        return new ImageView(wr).getImage();
+//    }
 }
